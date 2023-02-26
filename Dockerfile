757707105962210c17527878c0a1495140edca7e
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-02-26 18:41:59 +0200

    base tomcat app and fix sql db

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@
 FROM tomcat:latest
 RUN  apt-get update &&  apt-get -y dist-upgrade
 
-
-#tomcat
+#ports - 8080 
 EXPOSE 8080 
+
+COPY ./tomcatdeploy ./webapps
+
 CMD  ["catalina.sh", "run"]
---
+++ gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloWorldApplication.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class);
+	}
+
+
+	@RequestMapping("/")
+	String helloWorld() {
+		return "Hello World!";
+	}
+
+}
+
