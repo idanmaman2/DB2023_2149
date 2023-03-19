d23147ca59c80e82b0db306f9079d2b2ed3a7e94
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-03-19 14:12:50 +0200

    backend - withall depndencies and name refactoring

---
+++ backend/src/main/java/com/api/cinematown/CinematownapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.cinematown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematownapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematownapiApplication.class, args);
+	}
+
+}
