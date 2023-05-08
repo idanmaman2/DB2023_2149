73d7da850b15eac8207f9fb5ff32c7652224cc24
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-05-09 00:41:12 +0300

    magic array

--- scripts/populate/scripts/maintnance_workes_populate.py
+++ scripts/populate/scripts/maintnance_workes_populate.py
@@ -3,6 +3,7 @@ from objects.costumer import Costumer
 from util import mycursor , mydb
 from random import sample,choices
 WORKERS_NUM = 300 
+CHANCES_TO_TYPE = [6,3,1]
 mycursor.execute("""SELECT registered_costumers.first_name ,
                  registered_costumers.last_name,
                  registered_costumers.email , 
@@ -11,9 +12,8 @@ mycursor.execute("""SELECT registered_costumers.first_name ,
                  FROM `registered_costumers` WHERE 1;""")
 costumers = list(map(lambda x : Costumer(*x) , mycursor.fetchall()))
 workers_id = map( lambda costumer : costumer.id , sample(costumers,300))
-workers = list(map(lambda obj : MaintenaceWorkers(obj[0],obj[1]), zip(workers_id , choices(list(MaintenaceWorkers.Roles) , weights=[6,3,1] , k=WORKERS_NUM))))
+workers = list(map(lambda obj : MaintenaceWorkers(obj[0],obj[1]), zip(workers_id , choices(list(MaintenaceWorkers.Roles) , weights=CHANCES_TO_TYPE , k=WORKERS_NUM))))
 print(*workers,sep="\n\n")
-print(*list(map(lambda x : tuple(x), workers)))
 worker_sql = tuple(map(lambda x : tuple(x), workers)) 
 for worker in workers : 
     try:
